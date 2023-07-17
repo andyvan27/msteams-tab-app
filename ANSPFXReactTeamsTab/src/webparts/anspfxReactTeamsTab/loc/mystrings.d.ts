4edcb03a18b36e99ec414b603ff1be4f994f8184
AndyN <121646012+andyvan27@users.noreply.github.com>
2023-07-17 12:26:00 +1000

    Completed SPFX Teams Tab samples

---
+++ ANSPFXReactTeamsTab/src/webparts/anspfxReactTeamsTab/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IAnspfxReactTeamsTabWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'AnspfxReactTeamsTabWebPartStrings' {
+  const strings: IAnspfxReactTeamsTabWebPartStrings;
+  export = strings;
+}
